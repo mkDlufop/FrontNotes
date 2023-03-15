8d21c3f60fd1e20d61d4dca2d3a5fe56714121da
mkDlufop <mkdlufop@qq.com>
2023-03-15 12:02:33 +0800

    add: 安装 vuepress && 完成 vuepress 基础配置

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  title: 'mkDlufop`s notes',
+  description: 'Just Hack',
+  themeConfig: {
+    // 导航栏配置
+    nav: [
+      { text: 'HTML', link: '/html' },
+      { text: 'CSS', link: '/css' },
+      {
+        text: 'JS',
+        link: '/js',
+        items: [
+          { text: 'Group1', link: '/g1' },
+          { text: 'Group2', link: '/g2' }
+        ]
+      },
+      { text: 'GitHub', link: 'https://github.com/mkDlufop/FrontNotes' },
+    ],
+    // 侧边栏配置
+    sidebar: [
+    ]
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,25 @@
+---
+home: true
+# heroImage: /hero.png
+heroText: mkDlufop`s notes
+tagline: 一个记录前端笔记的笔记本
+actionText: 立刻阅读 →
+actionLink: /html
+features:
+- title: HTML
+  details: 必不可少的骨骼
+  link: /html
+- title: CSS
+  details: 令人沉醉的容貌
+  link: /css
+- title: JS
+  details: 极度结实的肌肉
+  link: /js
+footer: MIT Licensed | Copyright © 2022-present mkDlufop
+---
+
+# STFW RTFM RTFSC
+
+## 机器永远是对的
+
+## 未测试代码永远是错的
